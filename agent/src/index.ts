d5085450a2c7fb04de5d6842f54a03d5b2c80876
Wavenode <WDiamond@wavenode.io>
2024-12-16 23:05:18 +0100

    Update index.ts

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -51,6 +51,8 @@ import readline from "readline";
 import { fileURLToPath } from "url";
 import yargs from "yargs";
 
+import {mainCharacter} from '../mainCharacter.ts'
+
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
@@ -192,7 +194,7 @@ export async function loadCharacters(
 
     if (loadedCharacters.length === 0) {
         elizaLogger.info("No characters found, using default character");
-        loadedCharacters.push(defaultCharacter);
+        loadedCharacters.push(mainCharacter);
     }
 
     return loadedCharacters;
@@ -565,7 +567,7 @@ const startAgents = async () => {
 
     let charactersArg = args.characters || args.character;
 
-    let characters = [defaultCharacter];
+    let characters = [mainCharacter];
 
     if (charactersArg) {
         characters = await loadCharacters(charactersArg);
